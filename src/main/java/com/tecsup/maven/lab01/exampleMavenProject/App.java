2b3ebcf28428ac03424647dde6935762399daa30
Alumno <Alumno@LAB04-PC10.TECSUP-TRUJ.LOCAL>
2022-09-12 11:24:23 -0500

    First commit

---
+++ src/main/java/com/tecsup/maven/lab01/exampleMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.lab01.exampleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
